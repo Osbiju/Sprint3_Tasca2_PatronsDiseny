cb6ffea59b01482334019ddf1bb30d6c7dc67cc2
Oscar <okeshik@protonmail.com>
2024-02-01 13:02:27 +0100

    n1 i n2 acabats, falta revisar

---
+++ src/main/java/ObserverExemple/SachinFan.java
@@ -0,0 +1,16 @@
+package ObserverExemple;
+
+public class SachinFan {
+
+    private String name;
+
+    public SachinFan(String name){
+        this.name = name;
+    }
+
+    public void announce(){
+        System.out.println(name + " notified");
+    }
+
+
+}
